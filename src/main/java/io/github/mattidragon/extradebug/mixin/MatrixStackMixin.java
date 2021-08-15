445085a38c9e0c9e8025f90da78577a75d1de5f3
MattiDragon <matias.koivikko@gmail.com>
2021-08-15 12:59:55 +0300

    update 0.1.0

--- src/main/java/io/github/mattidragon/extradebug/mixin/MatrixStackMixin.java
+++ src/main/java/io/github/mattidragon/extradebug/mixin/MatrixStackMixin.java
@@ -1,6 +1,7 @@
 package io.github.mattidragon.extradebug.mixin;
 
-import io.github.mattidragon.extradebug.MatrixStackAction;
+import io.github.mattidragon.extradebug.ExtraDebug;
+import io.github.mattidragon.extradebug.MatrixStackUser;
 import io.github.mattidragon.extradebug.access.MatrixStackData;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,13 +9,16 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.transformer.meta.MixinMerged;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Objects;
 
 @Mixin(MatrixStack.class)
 public class MatrixStackMixin implements MatrixStackData {
     @Unique
-    private final ArrayList<MatrixStackAction> callers = new ArrayList<>();
+    private final ArrayList<MatrixStackUser> users = new ArrayList<>();
     
     @Inject(method = "push", at = @At("HEAD"))
     private void onPush(CallbackInfo ci) {
@@ -27,16 +31,44 @@ public class MatrixStackMixin implements MatrixStackData {
     }
     
     private void handle(boolean isPush) {
-        StackTraceElement caller = Thread.currentThread().getStackTrace()[4];
-        String method = caller.getClassName() + "." + caller.getMethodName();
-        boolean existed = callers.remove(new MatrixStackAction(method, !isPush));
-        if (!existed) {
-            callers.add(new MatrixStackAction(method, isPush));
+        StackWalker walker = StackWalker
+                .getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
+        Method method = walker.walk(frames -> frames
+                .map(frame -> {
+                    try {
+                        return frame.getDeclaringClass().getDeclaredMethod(frame.getMethodName(), frame.getMethodType().parameterArray());
+                    } catch (NoSuchMethodException e) {
+                        return null;
+                    }
+                })
+                .skip(3)
+                .findFirst())
+                .orElseThrow();
+        
+        
+        MatrixStackUser user = null;
+        for (int i = users.size() - 1; i >= 0; i--) {
+            MatrixStackUser candidate = users.get(i);
+            if (Objects.equals(candidate.getClazz(), method.getDeclaringClass().getTypeName())) {
+                user = candidate;
+                break;
+            }
         }
+        
+        if (user == null) users.add(user = new MatrixStackUser(method.getDeclaringClass().getTypeName()));
+        
+        String name = method.getName();
+        MixinMerged mixin = method.getAnnotation(MixinMerged.class);
+        if (mixin != null) {
+            name += " from mixin " + mixin.mixin();
+        }
+        
+        if (user.addMethod(name, isPush))
+            users.remove(user);
     }
     
     @Override
-    public MatrixStackAction[] getProblems() {
-        return callers.toArray(new MatrixStackAction[0]);
+    public MatrixStackUser[] getUsers() {
+        return users.toArray(new MatrixStackUser[0]);
     }
 }
--- src/main/java/io/github/mattidragon/extradebug/mixin/WorldRendererMixin.java
+++ src/main/java/io/github/mattidragon/extradebug/mixin/WorldRendererMixin.java
@@ -1,7 +1,7 @@
 package io.github.mattidragon.extradebug.mixin;
 
 import io.github.mattidragon.extradebug.ExtraDebug;
-import io.github.mattidragon.extradebug.MatrixStackAction;
+import io.github.mattidragon.extradebug.MatrixStackUser;
 import io.github.mattidragon.extradebug.access.MatrixStackData;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.math.MatrixStack;
@@ -11,20 +11,27 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import static io.github.mattidragon.extradebug.ExtraDebug.ERROR_LOG;
+
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
     @Inject(method = "checkEmpty", at = @At(value = "NEW", target = "java/lang/IllegalStateException"))
     private void betterError(MatrixStack matrices, CallbackInfo ci) {
-        ExtraDebug.ERROR_LOG.println("Matrix stack error: Pose stack not empty");
-        ExtraDebug.ERROR_LOG.println("Methods that might be related:");
-        for (MatrixStackAction action : ((MatrixStackData)matrices).getProblems()) {
-            ExtraDebug.ERROR_LOG.print(action.isPush() ? "push " : "pop  ");
-            ExtraDebug.ERROR_LOG.println(action.method());
+        ERROR_LOG.println("Matrix stack error: Pose stack not empty");
+        ERROR_LOG.println("Suspected classes:");
+        
+        for (MatrixStackUser user : ((MatrixStackData)matrices).getUsers()) {
+            ERROR_LOG.printf("  %s: %s %s%n", user.getClazz(), Math.abs(user.getActions()), user.getActions() < 0 ? "pops" : "pushes");
+            ERROR_LOG.println("    Suspected methods:");
+            
+            for (MatrixStackUser.Method method : user.getMethods()) {
+                ERROR_LOG.println("      " + (method.isPush() ? "push " : "pop  ") + method.name());
+            }
         }
     }
     
     @ModifyConstant(method = "checkEmpty")
     private String changeMessage(String message) {
-        return "Pose stack not empty".equals(message) ? (message + ". Please check extra-debug.log.") : message;
+        return "Pose stack not empty".equals(message) ? (message + ". Please check extra-debug.log") : message;
     }
 }
